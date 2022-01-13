d46e5d67948458a186d282c529b8fe87aceb20b3
Erin Sheldon <erin.sheldon@gmail.com>
2022-01-13 13:46:26 -0500

    bug in name

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = glob('bin/*')
 scripts = [s for s in scripts if '~' not in s]
 
 setup(
-    name="ngmix",
+    name="pizza-cutter-masking",
     author="Erin Sheldon, Matt Becker",
     url="https://github.com/beckermr/pizza-cutter-masking",
     description="make healsparse maps for DES Y6 pizza cutter/metadetect",
