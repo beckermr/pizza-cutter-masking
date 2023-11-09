3e93a65015c298e8e302ce54159ef21d6498c527
Eli Rykoff <erykoff@stanford.edu>
2023-11-09 15:37:05 -0800

    Update to use bit_packed masks from healsparse 1.8

--- pizza_cutter_masking/maps.py
+++ pizza_cutter_masking/maps.py
@@ -3,7 +3,7 @@ from .constants import (
 )
 
 
-def geom_to_map(geom_list, nside):
+def geom_to_map(geom_list, nside, use_bool=False):
     """
     Create a healsparse map from the input list of geometry primitives
 
@@ -11,6 +11,10 @@ def geom_to_map(geom_list, nside):
     ----------
     geom_list: [geom]
         E.g. a list of Circle or Polygon
+    nside : `int`
+        Healpix nside of the map.
+    use_bool : `boolean`, optional
+        Use a boolean bit-packed mask?
 
     Returns
     -------
@@ -24,15 +28,30 @@ def geom_to_map(geom_list, nside):
     if nside is None:
         nside = NSIDE
 
+    if use_bool:
+        dtype = bool
+        val = True
+        bit_packed = True
+        sentinel = False
+    else:
+        dtype = np.int16
+        val = 1
+        bit_packed = False
+        sentinel = 0
+
     hmap = healsparse.HealSparseMap.make_empty(
         nside_coverage=NSIDE_COVERAGE,
         nside_sparse=nside,
-        dtype=np.int16,
-        sentinel=0,
+        dtype=dtype,
+        sentinel=sentinel,
+        bit_packed=bit_packed,
     )
 
     for g in tqdm(geom_list):
         pixels = g.get_pixels(nside=nside)
-        hmap[pixels] |= g.value
+        if use_bool:
+            hmap[pixels] = True
+        else:
+            hmap[pixels] |= g.value
 
     return hmap
